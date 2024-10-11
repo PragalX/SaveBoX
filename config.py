838c5970fa092b28f2ab729e6d40689c02ab2104
AkhandaNandTripathi <168406488+PragalX@users.noreply.github.com>
2024-10-11 09:52:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ BOT_TOKEN = getenv("BOT_TOKEN", "7260141871:AAFxJ538hnVqfu3DqelIg6JfsiW1ZwsAhnQ"
 OWNER_ID = list(map(int, getenv("OWNER_ID", "7704179769").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://PihuMusic:PihuMusic@cluster0.w3eiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "-1002332668328")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1001573605549"))
